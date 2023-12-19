90c0906c519cc632fec680f31662657537e957ed
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-12-19 22:59:04 +0100

    proper halton dimensions

--- prng/halton_prng.py
+++ prng/halton_prng.py
@@ -4,11 +4,16 @@ from .prng import PRNG
 
 class HALTON_PRNG(PRNG):
     def __init__(self, seed) -> None:
-        self._prng = qmc.Halton(d=1, seed=seed)
+        self._prng = qmc.Halton(d=10, seed=seed)
+        self._dim = 10
+        self._seed = seed
 
     def __str__(self) -> str:
-        return f"halton_prng_{super().__str__()}"
+        return f"halton_{super().__str__()}"
 
     def std_normal(self, dim: int):
-        samples = self._prng.random(n=dim)
-        return norm.ppf(samples).reshape((1, dim))[0]
+        if self._dim != dim:
+            self._dim = dim
+            self._prng = qmc.Halton(d=dim, seed=self._seed)
+        samples = self._prng.random(n=1)
+        return norm.ppf(samples)[0]
