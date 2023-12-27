237d83ab4f19d7085ae6ccab16514e24d050437a
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-12-27 13:15:08 +0100

    chaospy implementation

--- prng/halton_prng.py
+++ prng/halton_prng.py
@@ -1,19 +1,15 @@
-from scipy.stats import qmc, norm
+from chaospy import Normal
 from .prng import PRNG
 
 
 class HALTON_PRNG(PRNG):
     def __init__(self, seed) -> None:
-        self._prng = qmc.Halton(d=10, seed=seed)
-        self._dim = 10
+        self._distr = Normal(0, 1)
         self._seed = seed
 
     def __str__(self) -> str:
         return f"halton_{super().__str__()}"
 
     def std_normal(self, dim: int):
-        if self._dim != dim:
-            self._dim = dim
-            self._prng = qmc.Halton(d=dim, seed=self._seed)
-        samples = self._prng.random(n=1)
-        return norm.ppf(samples)[0]
+        samples = self._distr.sample(dim, rule="halton", seed=self._seed)
+        return samples
--- prng/sobol_prng.py
+++ prng/sobol_prng.py
@@ -1,19 +1,15 @@
-from scipy.stats import qmc, norm
+from chaospy import Normal
 from .prng import PRNG
 
 
 class SOBOL_PRNG(PRNG):
     def __init__(self, seed) -> None:
-        self._prng = qmc.Sobol(d=10, seed=seed)
-        self._dim = 10
+        self._distr = Normal(0, 1)
         self._seed = seed
 
     def __str__(self) -> str:
         return f"sobol_{super().__str__()}"
 
     def std_normal(self, dim: int):
-        if self._dim != dim:
-            self._dim = dim
-            self._prng = qmc.Sobol(d=dim, seed=self._seed)
-        samples = self._prng.random(n=1)
-        return norm.ppf(samples)[0]
+        samples = self._distr.sample(dim, rule="sobol", seed=self._seed)
+        return samples
