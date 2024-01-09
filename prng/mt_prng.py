6a70e07f19175cec90714c2b10ef3b624aff3687
s.lukawski@student.utwente.nl <01169205@pw.edu.pl>
2024-01-09 03:26:00 +0100

    Added default values to logger in mt and xoroshiro

--- prng/mt_prng.py
+++ prng/mt_prng.py
@@ -10,7 +10,7 @@ class MtPrng(Prng):
 
     name = "mt"
 
-    def __init__(self, seed, dim, logger):
+    def __init__(self, seed, dim, logger=None):
         self._prng = np.random.RandomState(seed)
 
     def __str__(self) -> str:
